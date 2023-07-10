e8e65183584f79733868f88173a9a4c3ac13ac93
angelcasadey <angel.goca.88@gmail.com>
2023-07-09 21:23:39 -0600

    Nombre del proyecto: my-app. Descripcion: version final del proyecto my-app

---
+++ src/main/java/com/angel/myapp/entity/TestEntity.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.angel.myapp.entity;
+
+/**
+ * @author angel
+ *
+ */
+public class TestEntity {
+	
+	/**
+	 * Propiedad de prueba para la muestra de compilacion del archivo testentity
+	 */
+	
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
